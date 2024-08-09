068b7254e3207705c30b74cb8e7dee75335ea0d9
PavloYusyk <pavloyusyk@gmail.com>
2024-08-09 20:16:17 +0300

    HW-3

--- HW/HW3/main.js
+++ HW/HW3/main.js
@@ -107,7 +107,7 @@ let products = [
 for (const product of products) {
     document.write(`<div class="product-card">
         <h3 class="product-title">${product.title}. Price - ${product.price}</h3>)
-        <img src="${product.image}" class="product-image">)
+        <img src="${product.image}" alt="${product.title}" class="product-image">)
     </div>`)
 
 }
